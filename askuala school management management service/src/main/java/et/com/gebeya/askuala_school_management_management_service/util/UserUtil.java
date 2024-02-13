6283fc7f01b63d262da5210adcf8dd5e0df47268
Abinet <abinettamiru28@gmail.com>
2024-02-13 14:26:26 +0300

    feat: user credentials can be send now through email

--- askuala school management management service/src/main/java/et/com/gebeya/askuala_school_management_management_service/util/UserUtil.java
+++ askuala school management management service/src/main/java/et/com/gebeya/askuala_school_management_management_service/util/UserUtil.java
@@ -1,21 +1,26 @@
 package et.com.gebeya.askuala_school_management_management_service.util;
 
+import et.com.gebeya.askuala_school_management_management_service.dto.requestdto.AddAccountRequestDto;
+import et.com.gebeya.askuala_school_management_management_service.dto.requestdto.PaymentRequestDto;
 import et.com.gebeya.askuala_school_management_management_service.dto.responsedto.UserDto;
 import et.com.gebeya.askuala_school_management_management_service.enums.Authority;
 import et.com.gebeya.askuala_school_management_management_service.model.Users;
 import et.com.gebeya.askuala_school_management_management_service.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+
+import static et.com.gebeya.askuala_school_management_management_service.util.Constant.ADD_ACCOUNT_TOPIC;
 
 @Component
 @RequiredArgsConstructor
 public class UserUtil {
    private final UsersRepository usersRepository;
    private final PasswordEncoder passwordEncoder;
-
+    private final KafkaTemplate<String, AddAccountRequestDto> addAccountRequestDtoKafkaTemplate;
     private static StringBuilder randomStringGenerator() {
         final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
         final int DEFAULT_LENGTH = 12;
@@ -43,14 +48,9 @@ public class UserUtil {
 
         usersRepository.save(users);
 
-        UserDto dto = UserDto.builder()
-                .userName(username)
-                .password(password)
-                .role(authority.name())
-                .email(email)
-                .build();
 
-        //TODO kafka send dto object
+        AddAccountRequestDto request = AddAccountRequestDto.builder().name(fName).email(email).username(username).password(password).build();
+        addAccountRequestDtoKafkaTemplate.send(ADD_ACCOUNT_TOPIC,request);
     }
 
     public void userDisabler(Users users)
